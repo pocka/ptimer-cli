277beb947104955c7b0ba0b204f652e2092cc7af
Shota FUJI <pockawoooh@gmail.com>
2024-08-27 22:03:38 +0900

    Remove files unrelated to CLI

--- web_builder/src/modules.d.ts
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2024 Shota FUJI <pockawoooh@gmail.com>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-declare module "*.gleam" {
-	export function main(): void;
-	export function story(args: unknown, ctx: unknown): Element;
-}
